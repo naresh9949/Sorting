20a21dcbd9b3093924ee17682a95f8925dbc192f
Naresh Kollipora <53150348+naresh9949@users.noreply.github.com>
2020-07-23 22:55:17 -0700

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,60 +1,76 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-void Merge(int a[],int l,int m,int r)
+
+void Merge(int arr[],int start,int mid,int end)
 {
-  int n1=m-l+1;
-  int n2=r-m;
-  int a1[n1],a2[n2],i,j,k;
-  for(i=0;i<n1;i++)
-    a1[i]=a[l+i];
-  for(i=0;i<n2;i++)
-    a2[i]=a[m+1+i];
-  i=0;
-  j=0;
-  k=l;
-  while(i<n1 && j<n2)
-  {
-    if(a1[i]>a2[j])
+    int n1 = mid-start+1;
+    int n2 = end-mid;
+    
+    int a[n1],b[n2];
+    
+    for(int i=0;i<n1;i++)
+    a[i]=arr[start+i];
+    
+    for(int i=0;i<n2;i++)
+    b[i] = arr[mid+i+1];
+    
+    int i=0,j=0,k=start;
+    
+    while(i<n1 && j<n2)
     {
-      a[k]=a2[j];
-      j++;
+        if(a[i]<b[j])
+        {
+            arr[k] = a[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = b[j];
+            j++;
+        }
+        
+        k++;
     }
-    else
+    
+    while(i<n1)
     {
-      a[k]=a1[i];
-      i++;
+        arr[k] = a[i];
+        i++;
+        k++;
     }
-    k++;
-  }
-  while(i<n1)
-  {
-    a[k]=a1[i];
-    i++;
-    k++;
-  }
-  while(j<n2)
-  {
-    a[k]=a2[j];
-    j++;
-    k++;
-  }
+    
+    while(j<n2)
+    {
+        arr[k] = b[j];
+        j++;
+        k++;
+    }
 }
-void Merge_sort(int a[],int l,int r)
+
+
+void Merge_Sort(int arr[],int start,int end)
 {
-  if(l<r)
-  {
-    int mid=(l+r)/2;
-    Merge_sort(a,l,mid);
-    Merge_sort(a,mid+1,r);
-    Merge(a,l,mid,r);
-  }
+    if(start<end)
+    {
+        int mid = (start+end)/2;
+        
+        Merge_Sort(arr,start,mid);
+        Merge_Sort(arr,mid+1,end);
+        
+        Merge(arr,start,mid,end);
+    }
 }
+
 int main()
 {
-  int a[]={1,2,0,3,4,0},i;
-  int n = sizeof(a)/sizeof(a[0]);
-  Merge_sort(a,0,n-1);
-  for(i=0;i<n;i++)
-    cout<<a[i]<<" ";
-  cout<<endl;
+    int a[] = {6,8,2,1,3,9};
+    
+    int n = sizeof(a)/sizeof(a[0]);
+    
+    Merge_Sort(a,0,n-1);
+    
+    for(int i=0;i<n;i++)
+    std::cout << a[i] << std::endl;
+    
 }
